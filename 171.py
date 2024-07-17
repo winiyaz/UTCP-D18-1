6276c8dd7b2f30590b40815823272d8926b3a8cb
winiyaz <winiyaz@proton.me>
2024-07-17 12:19:59 -0700

    - Setup up

--- 171.py
+++ 171.py
@@ -1,12 +1,33 @@
 # Work from 171
-
+import turtle
 from turtle import Turtle, Screen
 
+# Turtle setup ---
 
 to = Turtle()  #Intitialize Turtle Class
 to.sc = Screen()  # Initialize Screen Class
-to.sc.register_shape("ax.gif")  # Initialize Shape
 to.color("coral")  # Setting color
+to.sc.register_shape("ax.gif")  # Register/ Initialize Shape
+to.shape("ax.gif")  # Use Shape
 to.shapesize(stretch_wid=3, stretch_len=3, outline=1) # Setting size params
 to.sc.bgcolor("#020617") # Set Bg Color
-to.sc.exitonclick()  # Exiting on click
+
+
+# Exercise
+
+def draw_shape(num_sides):
+	angle = 360 / num_sides
+	for _ in range(num_sides):
+		to.forward(100)
+		to.right(angle)
+
+for shape_sides_n in range(3,11):
+	draw_shape(shape_sides_n)
+
+
+
+
+
+# --- Exiting on click function
+to.sc.exitonclick()
+# ---
