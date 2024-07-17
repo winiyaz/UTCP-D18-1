6676c4b0476dd377e5da3ce589243917fc1d6d87
winiyaz <winiyaz@proton.me>
2024-07-17 12:56:18 -0700

    -RefactorPanty - Add initializer module

--- wo172.py
+++ wo172.py
@@ -1,23 +1,15 @@
 # Work for 172
 
-import random as rp
 from turtle import Turtle, Screen
+
+from toinit import to_init as toi
 
 # Turtle setup ---
 
 to = Turtle()  # Intitialize Turtle Class
 to.sc = Screen()  # Initialize Screen Class
 
-def to_init():
-	to.color("coral")  # Setting color
-	to.sc.register_shape("ax.gif")  # Register/ Initialize Shape
-	to.shape("ax.gif")  # Use Shape
-	to.shapesize(stretch_wid=3, stretch_len=3, outline=1)  # Setting size params
-	to.sc.bgcolor("#020617")  # Set Bg Color
-
-
-# Intialize the function
-to_init()
+toi()  # Initializer imported
 
 # --- Exiting on click function
 to.sc.exitonclick()
