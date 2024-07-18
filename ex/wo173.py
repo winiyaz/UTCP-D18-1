1792388cfa9d2d5df5f3b1e0f67145b22710ec18
winiyaz <winiyaz@proton.me>
2024-07-17 19:30:36 -0700

    - 174 RandomColor Works

--- ex/wo173.py
+++ ex/wo173.py
@@ -22,26 +22,38 @@ to_init()
 # Main Here
 
 # Setting The color - taken from https://trinket.io/docs/colors
-colo = [
-	"lime",
-	"deep pink",
-	"deep sky blue",
-	"yellow",
-	"magenta",
-	"lavender blush"
-]
+# colo = [
+# 	"lime",
+# 	"deep pink",
+# 	"deep sky blue",
+# 	"yellow",
+# 	"magenta",
+# 	"lavender blush"
+# ]
 
 # Robot movement
 
 directions = [0, 90, 180, 270]  # Directions
 to.speed("fastest")
 
+# Randomizing colors
+to.sc.colormode(255)
+
+def rando_colo():
+	"""Random Color generator"""
+	r = rp.randint(0,255)
+	g = rp.randint(0,255)
+	b = rp.randint(0,255)
+	ra_co = (r,g,b)
+	return ra_co
+
+
 for _ in range(200):
-	to.forward(100)
+	to.forward(30)
 	to.backward(20)
 	to.setheading(rp.choice(directions))
-	to.color(rp.choice(colo))
-	to.pensize(rp.randint(1, 10))
+	to.color(rando_colo())
+	to.pensize(rp.randint(5, 15))
 
 # --- Exiting on click function
 to.sc.exitonclick()
